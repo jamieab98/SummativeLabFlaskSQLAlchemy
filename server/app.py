a83978a4e21d811785d77e2a997575bf87ce76b1
Jamie Bryson <jamieab98@gmail.com>
2026-03-12 19:48:50 -0400

    created the /workouts route

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, make_response
+from flask import Flask, jsonify
 from flask_migrate import Migrate
 
-from models import db
+from models import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -12,6 +12,14 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 #Routes
+@app.route("/workouts")
+def get_workouts():
+    workouts = Workout.query.all()
+    workout_list = []
+    for workout in workouts:
+        workout_dict = {'id': workout.id, 'date': workout.date.isoformat(), 'duration in minutes': workout.duration_minutes, 'notes': workout.notes}
+        workout_list.append(workout_dict)
+    return jsonify(workout_list), 200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
