e3c8927fa713423697ef449bc793d16a256e0849
Jamie Bryson <jamieab98@gmail.com>
2026-03-12 19:12:51 -0400

    Fine tuned the validators and added in the table constraint

--- server/models.py
+++ server/models.py
@@ -10,24 +10,13 @@ class WorkoutExercises(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     workout_id = db.Column(db.Integer, db.ForeignKey('workouts.id'))
     exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.id'))
-    reps = db.Column(db.Integer)
+    reps = db.Column(db.Integer, nullable=False)
     duration_seconds = db.Column(db.Integer)
 
-    @validates('reps')
-    def validates_reps(self, key, reps):
-        if reps is None:
-            raise ValueError("Please enter rep count")
-        if reps <= 0:
-            raise ValueError("Rep value must be greater than 0")
-        return reps
-    
-    @validates('duration_seconds')
-    def validates_duration_seconds(self, key, duration_seconds):
-        if duration_seconds is None:
-            raise ValueError("Please enter duration in seconds")
-        if duration_seconds <= 0:
-            raise ValueError("Duration values must be more than 0 seconds")
-        return duration_seconds
+    __table_args__ = (
+        db.CheckConstraint("reps > 0"),
+        db.CheckConstraint("duration_seconds > 0")
+    )
 
     workout = db.relationship('Workout', back_populates='workoutexercises')
     exercise = db.relationship('Exercise', back_populates='workoutexercises')
@@ -45,6 +34,12 @@ class Exercise(db.Model):
         if name is None or name == "":
             raise ValueError("Exercise name is required")
         return name
+    
+    @validates('category')
+    def validates_category(self, key, category):
+        if category != 'Strength' and category != 'Cardio':
+            raise ValueError("Category must be 'Strength' or 'Cardio'")
+        return category
 
     workoutexercises = db.relationship('WorkoutExercises', back_populates='exercise')
 
