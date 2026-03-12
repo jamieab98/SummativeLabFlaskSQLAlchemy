d00b21597c31694f6b39c0537925ea551f56cac2
Jamie Bryson <jamieab98@gmail.com>
2026-03-12 18:59:24 -0400

    Created validators

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+from datetime import date
 db = SQLAlchemy()
 
 #Models
@@ -12,6 +13,22 @@ class WorkoutExercises(db.Model):
     reps = db.Column(db.Integer)
     duration_seconds = db.Column(db.Integer)
 
+    @validates('reps')
+    def validates_reps(self, key, reps):
+        if reps is None:
+            raise ValueError("Please enter rep count")
+        if reps <= 0:
+            raise ValueError("Rep value must be greater than 0")
+        return reps
+    
+    @validates('duration_seconds')
+    def validates_duration_seconds(self, key, duration_seconds):
+        if duration_seconds is None:
+            raise ValueError("Please enter duration in seconds")
+        if duration_seconds <= 0:
+            raise ValueError("Duration values must be more than 0 seconds")
+        return duration_seconds
+
     workout = db.relationship('Workout', back_populates='workoutexercises')
     exercise = db.relationship('Exercise', back_populates='workoutexercises')
 
@@ -23,6 +40,12 @@ class Exercise(db.Model):
     category = db.Column(db.String)
     equipment_needed = db.Column(db.Boolean)
 
+    @validates('name')
+    def validates_name(self, key, name):
+        if name is None or name == "":
+            raise ValueError("Exercise name is required")
+        return name
+
     workoutexercises = db.relationship('WorkoutExercises', back_populates='exercise')
 
 class Workout(db.Model):
@@ -32,5 +55,19 @@ class Workout(db.Model):
     date = db.Column(db.Date)
     duration_minutes = db.Column(db.Integer)
     notes = db.Column(db.Text)
+
+    @validates('date')
+    def validates_date(self, key, value):
+        if value is None:
+            raise ValueError("Please provide a date")
+        if value > date.today():
+            raise ValueError("Date cannot be beyond today's date")
+        return value
+    
+    @validates('notes')
+    def validates_notes(self, key, notes):
+        if notes and len(notes) > 1024:
+            raise ValueError("Please provide a shorter note")
+        return notes
 
     workoutexercises = db.relationship('WorkoutExercises', back_populates='workout')
