78a17ce1837a857c8ff08b83621d143171f6fd97
Jamie Bryson <jamieab98@gmail.com>
2026-03-12 17:15:11 -0400

    Created the Workout model

--- server/models.py
+++ server/models.py
@@ -10,3 +10,11 @@ class Exercise(db.Model):
     name = db.Column(db.String)
     category = db.Column(db.String)
     equipment_needed = db.Column(db.Boolean)
+
+class Workout(db.Model):
+    __tablename__ = 'workouts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date) #not sure if this is right for the date
+    duration_minutes = db.Column(db.Integer)
+    notes = db.Column(db.Text) #should this be db.String?
