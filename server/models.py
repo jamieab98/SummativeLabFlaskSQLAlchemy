84a4d74207c1a92f007cf2f5e03acf3ec3bd5487
Jamie Bryson <jamieab98@gmail.com>
2026-03-12 17:58:29 -0400

    Turn WorkoutExercises into a class instead of just a join table

--- server/models.py
+++ server/models.py
@@ -3,6 +3,15 @@ from sqlalchemy.orm import validates
 db = SQLAlchemy()
 
 #Models
+class WorkoutExercises(db.Model):
+    __tablename__ = 'workout_exercises'
+
+    id = db.Column(db.Integer, primary_key=True)
+    workout_id = db.Column(db.Integer, db.ForeignKey('workouts.id'))
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercise.id'))
+    reps = db.Column(db.Integer)
+    duration_seconds = db.Column(db.Integer)
+
 class Exercise(db.Model):
     __tablename__ = 'exercises'
 
@@ -10,6 +19,8 @@ class Exercise(db.Model):
     name = db.Column(db.String)
     category = db.Column(db.String)
     equipment_needed = db.Column(db.Boolean)
+
+    workouts = db.relationship('Workout', back_populates='exercises', secondar=WorkoutExercises)
 
 class Workout(db.Model):
     __tablename__ = 'workouts'
@@ -19,10 +30,4 @@ class Workout(db.Model):
     duration_minutes = db.Column(db.Integer)
     notes = db.Column(db.Text)
 
-workout_exercises = db.Table('WorkoutExercises', 
-                           db.Column('id', db.Integer, primary_key=True),
-                           db.Column('workout_id', db.Integer, db.ForeignKey('workouts.id')),
-                           db.Column('exercise_id', db.Integer, db.ForeignKey('exercises.id')),
-                           db.Column('reps', db.Integer),
-                           db.Column('duration_seconds', db.Integer)
-                           )
+    exercises = db.relationship('Exercises', back_populates='workouts', secondary=WorkoutExercises)
